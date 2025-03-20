dbc6f321a1233f09f0bdae2d451f9571a3f5c2e2
AlbertKistowski <118359839+Bercik4200@users.noreply.github.com>
2025-03-20 22:17:36 +0100

    Add files via upload

---
+++ app_dashboard.py
@@ -0,0 +1,30 @@
+import sqlite3
+import os
+import pandas as pd # type: ignore
+import plotly.express as px
+import streamlit as st # type: ignore
+
+
+st.set_page_config(page_title="Analiza użytkowników", layout="wide")
+
+db_path = os.path.join(os.getcwd(), "users_data.db")
+conn = sqlite3.connect(db_path)
+
+df = pd.read_sql("SELECT * FROM users_activity", conn)
+conn.close()
+
+st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
+st.write("Podgląd danych:", df.head())
+
+st.set_page_config(page_title="Analiza użytkowników", layout="wide")
+st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
+
+#Filtracja
+selected_country = st.selectbox("Wybierz kraj:", df["country"].unique())
+filtered_df = df[df["country"] == selected_country]
+
+fig = px.line(filtered_df, x="date", y="screen_time_min", title=f"Czas spędzony w aplikacji - {selected_country}")
+st.plotly_chart(fig)
+
+st.write("Szczegółowe dane:")
+st.dataframe(filtered_df)
