392e7a00c76d4010d0a6397f89cb2c33442c6268
AlbertKistowski <118359839+Bercik4200@users.noreply.github.com>
2025-03-20 22:29:22 +0100

    Update app_dashboard.py
    Wcięcia zmienić

--- app_dashboard.py
+++ app_dashboard.py
@@ -13,23 +13,23 @@ st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
 if not os.path.exists(db_path): 
   st.error("Plik `users_data.db` nie został znaleziony! Upewnij się, że dodałeś go do repozytorium.")
 else:
-conn = sqlite3.connect(db_path)
+  conn = sqlite3.connect(db_path)
 
-df = pd.read_sql("SELECT * FROM users_activity", conn)
-conn.close()
+  df = pd.read_sql("SELECT * FROM users_activity", conn)
+  conn.close()
 
-st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
-st.write("Podgląd danych:", df.head())
-
-st.set_page_config(page_title="Analiza użytkowników", layout="wide")
-st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
-
-#Filtracja
-selected_country = st.selectbox("Wybierz kraj:", df["country"].unique())
-filtered_df = df[df["country"] == selected_country]
-
-fig = px.line(filtered_df, x="date", y="screen_time_min", title=f"Czas spędzony w aplikacji - {selected_country}")
-st.plotly_chart(fig)
-
-st.write("Szczegółowe dane:")
-st.dataframe(filtered_df)
+  st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
+  st.write("Podgląd danych:", df.head())
+  
+  st.set_page_config(page_title="Analiza użytkowników", layout="wide")
+  st.title("Analiza Aktywności Użytkowników Aplikacji Mobilnej")
+  
+  #Filtracja
+  selected_country = st.selectbox("Wybierz kraj:", df["country"].unique())
+  filtered_df = df[df["country"] == selected_country]
+  
+  fig = px.line(filtered_df, x="date", y="screen_time_min", title=f"Czas spędzony w aplikacji - {selected_country}")
+  st.plotly_chart(fig)
+  
+  st.write("Szczegółowe dane:")
+  st.dataframe(filtered_df)
